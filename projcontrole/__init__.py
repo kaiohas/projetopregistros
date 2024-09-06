ad6cb21395be69744da66cc7ca0380736d62f6c0
kaiohas <106276694+kaiohas@users.noreply.github.com>
2024-09-05 21:31:49 -0300

    Update __init__.py

--- projcontrole/__init__.py
+++ projcontrole/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
+
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'ba79b37d00047525a5e833980f864e91'
@@ -18,4 +21,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 #Fica em baixo pois o init precisa chamar as rotas
+
+
+from projcontrole import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de Dados criada')
+else:
+    print('Base de dados Existente')
+
 from projcontrole import routes
